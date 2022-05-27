9efee4de533e67d6649979c261c5f8babd833ccc
Andre Gomez <andregc1983@gmail.com>
2022-05-26 21:35:57 -0300

    Teste de validação de corpo de respostas

--- src/test/java/com/concessionaria/gomez/CadastroCarroIT.java
+++ src/test/java/com/concessionaria/gomez/CadastroCarroIT.java
@@ -1,4 +1,50 @@
 package com.concessionaria.gomez;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CadastroCarroIT {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void deveRetornarStatus200_QuandoConsultarCarros() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        RestAssured.given()
+                .basePath("/carros")
+                .port(port)
+                .accept(ContentType.JSON)
+            .when()
+                .get()
+            .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    /*Método para validação do corpo de resposta*/
+
+    @Test
+    public void deveConter14Carros() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        RestAssured.given()
+                .basePath("/carros")
+                .port(port)
+                .accept(ContentType.JSON)
+        .when()
+                .get()
+        .then()
+                .body("", Matchers.hasSize(14))
+                .body("modelo", Matchers.hasItems("Fiesta", "ASX"));
+    }
 }
