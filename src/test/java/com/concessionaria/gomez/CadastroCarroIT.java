814508c850f1872cf5d1863bc1f93d066aba7ef6
Andre Gomez <andregc1983@gmail.com>
2022-05-30 22:59:23 -0300

    Refatoração do teste de Retorno de Status com carro existente

--- src/test/java/com/concessionaria/gomez/CadastroCarroIT.java
+++ src/test/java/com/concessionaria/gomez/CadastroCarroIT.java
@@ -2,6 +2,9 @@ package com.concessionaria.gomez;
 
 import com.concessionaria.gomez.domain.model.Carro;
 import com.concessionaria.gomez.domain.repository.CarroRepository;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import com.concessionaria.gomez.util.ResourceUtils;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
@@ -21,6 +24,8 @@ import com.concessionaria.gomez.util.DatabaseCleaner;
 @TestPropertySource("/application-test.properties")
 public class CadastroCarroIT {
 
+    private static final int CARRO_ID_INEXISTENTE = 100;
+
     @LocalServerPort
     private int port;
 
@@ -30,11 +35,18 @@ public class CadastroCarroIT {
     @Autowired
     private CarroRepository carroRepository;
 
+    private Carro carroMercedes;
+    private int quantidadeCarrosCadastrados;
+    private String jsonCorretoCarroAudi;
+
     @BeforeEach
     public void setUp(){
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/carros";
+
+        jsonCorretoCarroAudi = ResourceUtils.getContentFromResource(
+                "/json/correto/carro-audi.json");
 
         databaseCleaner.clearTables();
         prepararDados();
@@ -51,43 +63,68 @@ public class CadastroCarroIT {
     }
 
     @Test
-    public void deveConter2Carros_QuandoConsultarCarros() { /*Método para validação do corpo de resposta*/
+    public void deveRetornarQuantidadeCorretaDeCarros_QuandoConsultarCarros() { /*Método para validação do corpo de resposta*/
         RestAssured.given()
             .accept(ContentType.JSON)
         .when()
             .get()
         .then()
-            .body("", Matchers.hasSize(2));
+            .body("", Matchers.hasSize(quantidadeCarrosCadastrados));
             /*.body("modelo", Matchers.hasItems("Fiesta", "ASX")); ---  Exemplos com outros filtros*/
     }
 
     @Test
     public void deveRetornarStatus201_QuandoCadastrarCarro() {
         RestAssured.given()
-                .body("{\"marca\": \"Ford\", \"modelo\" : \"Escort\", \"compra\" : \"24300\", \"ano\" : \"1997\"}")
-                .contentType(ContentType.JSON)
-                .accept(ContentType.JSON)
-            .when()
-                .post()
-            .then()
-                .statusCode(HttpStatus.CREATED.value());
+            .body(jsonCorretoCarroAudi)
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+        .when()
+            .post()
+        .then()
+            .statusCode(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    public void deveRetornarRespostasEStratusCorretos_QuandoConsultarCarroExistente(){
+        RestAssured.given()
+            .pathParam("carroId", carroMercedes.getId())
+            .accept(ContentType.JSON)
+        .when()
+            .get("/{carroId}")
+        .then()
+            .statusCode(HttpStatus.OK.value())
+            .body("modelo", equalTo(carroMercedes.getModelo()));
+    }
+
+    @Test
+    public void deveRetorbarStatus404_QuandoConsultarcarroInexistente(){
+        RestAssured.given()
+            .pathParam("carroId", CARRO_ID_INEXISTENTE)
+            .accept(ContentType.JSON)
+        .when()
+            .get("/{carroId}")
+        .then()
+            .statusCode(HttpStatus.NOT_FOUND.value());
     }
 
     /*------------- Método para inserir massas de dados para os testes rodarem ----------*/
 
     private void prepararDados(){
-        Carro carro1 = new Carro();
-        carro1.setMarca("Honda");
-        carro1.setModelo("Civic");
-        carro1.setAno(2018);
-        carro1.setCompra(98000.00);
-        carroRepository.save(carro1);
+        carroMercedes = new Carro();
+        carroMercedes.setMarca("Mercedes");
+        carroMercedes.setModelo("Classe A");
+        carroMercedes.setAno(2022);
+        carroMercedes.setCompra(319900.00);
+        carroRepository.save(carroMercedes);
 
-        Carro carro2 = new Carro();
-        carro2.setMarca("Audi");
-        carro2.setModelo("A4");
-        carro2.setAno(2022);
-        carro2.setCompra(15000.00);
-        carroRepository.save(carro2);
+        Carro carroBMW = new Carro();
+        carroBMW.setMarca("BMW");
+        carroBMW.setModelo("M5");
+        carroBMW.setAno(2018);
+        carroBMW.setCompra(150000.00);
+        carroRepository.save(carroBMW);
+
+        quantidadeCarrosCadastrados = (int) carroRepository.count();
     }
 }
